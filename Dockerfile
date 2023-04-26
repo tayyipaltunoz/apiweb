687405e85f7d9ba260fba487ff3c938ff90ca325
tayyipaltunoz <tayyip.altunoz@hotmail.com>
2023-04-26 14:46:07 +0300

    docker

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# base image
+FROM python:3.11-alpine
+
+# set working directory
+WORKDIR /app
+
+# copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# install dependencies
+RUN apk update && apk add --no-cache gcc musl-dev linux-headers
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+# copy the content of the local src directory to the working directory
+COPY . /app
+
+# command to run on container start
+CMD [ "python", "./app.py" ]
+
+
+#docker build -t my-flask-app .
+#docker run -p 5001:5001 my-flask-app
+
--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 import requests
 import pandas as pd
 import json
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def search_api():
     if request.method == 'POST':
         # Kullanıcının girdiği değeri alalım
         query = request.form['query']
-        
+
         # REST API'ye istek gönderelim
         response = requests.get('https://fakestoreapi.com/products/' + query)
-        
+
         # API yanıtını bir değişkene ata
         try:
-            result = response.json()  
-    
+            result = response.json()
+
             # Sonucu bir Flask şablonunda kullanmak için değişkeni geri döndür
-            return render_template('index.html', result=result)  
+            return render_template('index.html', result=result)
         except Exception as ex:
             print(f'Sonuç bulunamadı. {ex}')
-            error = (f'Sonuç bulunamadı. {ex}') 
-            return render_template('index.html', error=error)  
-    
-    
-    # GET istekleri için basit bir HTML formu döndürelim
-    
+            error = (f'Sonuç bulunamadı. {ex}')
+            return render_template('index.html', error=error)
+
+        # GET istekleri için basit bir HTML formu döndürelim
+
     return render_template("index.html")
 
+
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
-
-
+    app.run(port=5001, debug=True)
